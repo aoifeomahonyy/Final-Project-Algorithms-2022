4d4cb8947979a6746c4f6b7d17eb40ab7640951f
aoife <aoife@LAPTOP-9T9QAGB2.Home>
2022-04-08 14:19:49 +0100

    Setting up constructor to read in transfers.txt in Transfers.java

--- FinalProject/Transfers.java
+++ FinalProject/Transfers.java
@@ -1,21 +1,36 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.io.File;
 
 public class Transfers {
-	
-	public String from_stop_id;
-	public String to_stop_id;
+
+	public int from_stop_id;
+	public int to_stop_id;
 	public String transfer_type;
 	public int min_transfer_time;
-	
-	Transfers(String from_stop_id, String to_stop_id, String transfer_type, int min_transfer_time)
-	{
-		this.from_stop_id = from_stop_id;
-		this.to_stop_id = to_stop_id;
-		this.transfer_type = transfer_type;
-		this.min_transfer_time = min_transfer_time;
-	}
 
+	public Transfers(String input) {
+		String[] inputVals = input.split(",");
+		this.from_stop_id = Integer.parseInt(inputVals[0]);
+		this.to_stop_id = Integer.parseInt(inputVals[1]);
+		this.transfer_type = inputVals[3];
+		this.min_transfer_time = Integer.parseInt(inputVals[3]);
+	}
+	
+	public String returnFromStopId()
+	{
+		return "" + this.from_stop_id;
+	}
+	
+	public String returnToStopId()
+	{
+		return "" + this.to_stop_id;
+	}
+	
+	public String returnTransferType()
+	{
+		return "" + this.transfer_type;
+	}
+	
+	public String returnMinTransferType()
+	{
+		return "" + this.min_transfer_time;
+	}
 }
